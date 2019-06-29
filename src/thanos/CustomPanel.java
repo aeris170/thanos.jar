719894608a8479a83a9fc0442373ccc2a4a87d28
aeris170 <aeris170@gmail.com>
2019-06-29 17:32:48 +0300

    Move files

--- src/thanos/CustomPanel.java
+++ src/thanos/CustomPanel.java
@@ -1,4 +1,4 @@
-package thanos.jar;
+package thanos;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
--- src/thanos/Main.java
+++ src/thanos/Main.java
@@ -1,4 +1,4 @@
-package thanos.jar;
+package thanos;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
